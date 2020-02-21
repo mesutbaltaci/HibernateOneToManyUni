a36cb6093cfe691d6cb678a894c91f87ba3376ca
mesutbaltaci <mesutbaltaci@hotmail.com>
2020-02-21 16:01:10 -0500

    read and delete course and reviews

---
+++ SF-08ONEtoManyUni/src/org/ms/main/GetCourseandReviewsDemo.java
@@ -0,0 +1,45 @@
+package org.ms.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.ms.entities.Course;
+import org.ms.entities.Instructor;
+import org.ms.entities.InstructorDetail;
+import org.ms.entities.Review;
+
+
+public class GetCourseandReviewsDemo {
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.buildSessionFactory();
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//start transaction
+			session.beginTransaction();
+			
+			int theId=10;
+			Course tempCourse = session.get(Course.class, theId);
+			
+			System.out.println("Course: " +tempCourse);
+			
+			System.out.println(tempCourse.getReviews());
+			session.getTransaction().commit();
+			System.out.println("Done");
+			
+						
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+}
